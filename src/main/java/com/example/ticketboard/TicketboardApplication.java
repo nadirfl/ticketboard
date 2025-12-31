d13eb4787d6eb82aa9f80618b86e0a07cdfa9e16
Agar <nadir-ferlin@hotmail.com>
2025-12-31 17:29:28 +0100

    Initial commit

---
+++ src/main/java/com/example/ticketboard/TicketboardApplication.java
@@ -0,0 +1,13 @@
+package com.example.ticketboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TicketboardApplication.class, args);
+	}
+
+}
